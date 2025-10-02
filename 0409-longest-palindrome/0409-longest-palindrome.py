e66c3e393cab1adba61dd17833704fa0840228f5
Hawi-net <hawinetzewde7@gmail.com>
2025-10-02 23:49:57 +0300

    Time: 3 ms (39.63%), Space: 18 MB (27.43%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+ 
+        c = Counter(s).values()
+
+        pairCounts = 2 * sum(x>>1 for x in c)
+
+        return pairCounts + (pairCounts < len(s))
